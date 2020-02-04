34fb80875a69bb5006f817cc395e6883b61f975a
NSD4rKn3SS <molnar.barnabas@z3systems.hu>
2020-02-04 16:02:37 +0100

    Fixed random number

--- js/function.js
+++ js/function.js
@@ -120,7 +120,7 @@ $(document).ready(function($) {
         gameScene.addChild(ulu);
 
         //Ellenfelek létrehozása és tömb létesítése az eltárolásukhoz
-        let numberOfScavs = randomInt(5,12),
+        let numberOfScavs = randomInt(6,12),
             spacing = 48,
             xOffset = 150,
             speed = 2,
