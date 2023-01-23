b16f8cc3a0b082d569135b2eb8465e9d42a445fe
NSD4rKn3SS <barnabas.molnar92@gmail.com>
2023-01-23 22:17:53 +0100

    Test 2

--- effectground/js/function.js
+++ effectground/js/function.js
@@ -1,4 +1,10 @@
-function startAnimation(image) {
+
+var anims = {
+    1: animation1(image),
+    2: animation2(image),
+    3: animation3(image)
+}
+function animation1(image) {
     pict2pix.animate({
         image: image,
         particleType: 'led-matrix',
@@ -7,6 +13,31 @@ function startAnimation(image) {
         idleTime: 5000,
         ledSize: 4
     });
+};
+function animation2(image) {
+    pict2pix.animate({
+        image: image,
+        numberOfParticles: 800,
+        horizontalSpeed: 1,
+        verticalSpeed: -1,
+        particleType: 'twisted-particle'
+    });
+};
+
+function animation3(image) {
+    pict2pix.animate({
+        image: image,
+        particleType: 'halftone', 
+        transitionTime: 10000,
+        idleTime: 4000,
+        color: 'rgb(0, 60, 90)', 
+        separation: 8
+    });
+};
+
+function startAnimation(image) {
+    var rand = Math.floor(Math.random() * 3) + 1;
+    anims[rand](image);
 };
 
 
