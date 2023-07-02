5c6ccfb808c1053cf60c5d7497f2e554daaf861d
NSD4rKn3SS <barnabas.molnar92@gmail.com>
2023-07-02 19:37:17 +0200

    steambg test initial

---
+++ steambgs/main.js
@@ -0,0 +1,50 @@
+
+let lat = '47.2309',
+    lng = '16.621';
+let openMeteoURL = 'https://api.open-meteo.com/v1/forecast?latitude='+lat+'&longitude='+lng+'&current_weather=true&timezone=auto';
+let weather,
+    formattedWeather;
+
+function updateWeather() {
+    $.getJSON(openMeteoURL, function(data) {
+        weather = data;
+        formattedWeather = weather['current_weather']['temperature']+'°C';
+    });
+}
+
+
+function updateDateTime() {
+    var date = new Date();
+  
+    var options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    var formattedDate = date.toLocaleDateString("en-US", options);
+    var formattedTime = date.toLocaleTimeString("en-US", { hour12: false });
+  
+    var dateTimeElement = document.getElementById("datetime");
+    dateTimeElement.innerHTML = formattedDate +'. '+ formattedTime +'<br>'+formattedWeather;
+}
+
+function updateBG() {
+    let strBackgroundDesktop = "./summersale2023/home_header_bg_day_notext.gif";
+    let curDate = new Date();
+    if ( curDate.getHours() >= 18 || curDate.getHours() < 6 ) {
+        strBackgroundDesktop = "./summersale2023/home_header_bg_night_notext.gif";
+    } else {
+        strBackgroundDesktop = "./summersale2023/home_header_bg_day_notext.gif"
+    }
+    $( '.page_background_holder' ).css( 'background-image', 'url("' + strBackgroundDesktop + '")' );
+}
+// Refresh date and time every second (1000 milliseconds)
+setInterval(updateDateTime, 1000);
+setInterval(updateWeather, 3600000);
+setInterval(updateBG, 3600000);
+
+$(document).ready(function () {
+    updateDateTime();
+    updateWeather();
+    updateBG();
+});
