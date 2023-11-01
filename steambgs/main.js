98d9a9c0633cde82165f1ec70f9aafbcd32f8e2f
Barnabás Molnár (NSD4rKn3SS) <barnabas.molnar92@gmail.com>
2023-11-01 11:20:26 +0100

    js refactor, remove unused lib

--- steambgs/main.js
+++ steambgs/main.js
@@ -1,144 +1,155 @@
-/*let matrixIP = '192.168.0.4';
-let matrixID = '27970';
-let matrixTEXT = '';
-let matrixColor = '\\calign\\#ff4900';*/
+class SteamBG {
+	param = this.getURLParameters()
+	locale = this.param.locale 		? this.param.locale : 'hu-HU' //en-US
+	lat = this.param.lat 			? this.param.lat 	: '47.2309'
+	lng = this.param.lng 			? this.param.lng 	: '16.621'
+	hour12set = this.param.hour12 	? this.param.hour12 : false
+	season = this.param.season 		? this.param.season : 'summer'
+	year = this.param.year 			? this.param.year 	: '2023'
+	openMeteoURL = 'https://api.open-meteo.com/v1/forecast?latitude='+this.lat+'&longitude='+this.lng+'&current_weather=true&timezone=auto'
+	weather = false
+	formattedWeather = false
+	timeofday = 'day'
+	bgs = {
+		'years' : ["2023"],
+		'seasons' : ["spring", "summer"]
+		
+	}
 
-function getURLParameters() {
-  var query = window.location.search.substring(1); // Exclude the '?' character
-  var params = {};
+	getURLParameters() {
+		let query = window.location.search.substring(1); // Exclude the '?' character
+		let params = {};
 
-  // Split the query string into an array of parameters
-  var pairs = query.split('&');
+		// Split the query string into an array of parameters
+		let pairs = query.split('&');
 
-  // Iterate over each parameter and split it into a key/value pair
-  for (var i = 0; i < pairs.length; i++) {
-    var pair = pairs[i].split('=');
-    var key = decodeURIComponent(pair[0]);
-    var value = decodeURIComponent(pair[1] || '');
+		// Iterate over each parameter and split it into a key/value pair
+		for (var i = 0; i < pairs.length; i++) {
+			let pair = pairs[i].split('=');
+			let key = decodeURIComponent(pair[0]);
+			let value = decodeURIComponent(pair[1] || '');
 
-    // Store the key/value pair in the params object
-    if (key) {
-      // If the key already exists, convert it into an array of values
-      if (params[key]) {
-        if (Array.isArray(params[key])) {
-          params[key].push(value);
-        } else {
-          params[key] = [params[key], value];
-        }
-      } else {
-        params[key] = value;
-      }
-    }
-  }
+			// Store the key/value pair in the params object
+			if (key) {
+				// If the key already exists, convert it into an array of values
+				if (params[key]) {
+					if (Array.isArray(params[key])) {
+						params[key].push(value);
+					} else {
+						params[key] = [params[key], value];
+					}
+				} else {
+					params[key] = value;
+				}
+			}
+		}
 
-  return params;
+		return params;
+	}
+
+	updateWeather() {
+		let fn = this;
+		$.getJSON(fn.openMeteoURL, function(data) {
+			fn.weather = data;
+			fn.formattedWeather = fn.weather['current_weather']['temperature']+'°C';
+		});
+	}
+
+	updateDateTime() {
+		let fn = this
+		let date = new Date();
+	  
+		let options = {
+		  year: "numeric",
+		  month: "long",
+		  day: "numeric",
+		};
+		let formattedDate = date.toLocaleDateString(fn.locale, options);
+		let formattedTime = date.toLocaleTimeString(fn.locale, { hour12: fn.hour12set });
+	  
+		let dateTimeElement = document.getElementById("datetime");
+		dateTimeElement.innerHTML = formattedDate +'<br>'+ formattedTime +' - '+fn.formattedWeather;
+
+		if ( date.getHours() >= 18 || date.getHours() < 6 ) { fn.timeofday = 'night' } else { fn.timeofday = 'day' }
+
+
+	}
+
+	updateBG(season, year) {
+		let fn = this
+
+		if (season && year) {
+		  //set default BG
+		  let strBackgroundDesktop = "./images/"+season+"sale/"+year+"/home_header_bg_day_notext.gif";
+	  
+		  //its night
+		  if (fn.timeofday === 'night') {
+			if (season === "summer") {
+			  strBackgroundDesktop = "./images/"+season+"sale/"+year+"/home_header_bg_night_notext.gif";
+			}
+			if (season === "spring") {
+			  strBackgroundDesktop = "./images/"+season+"sale/"+year+"/page_bg_english.gif";
+			} else {
+			  strBackgroundDesktop = "./summersale2023/home_header_bg_night_notext.gif";
+			}
+			$('.page_pattern_holder').addClass(season+' y'+year);
+			$('.page_pattern_holder').addClass('night');
+		  }
+	  
+		  //its day
+		  else {
+			if (season === "summer") {
+			  strBackgroundDesktop = "./images/" + season + "sale/" + year + "/home_header_bg_day_notext.gif";
+			}
+			if (season === "spring") {
+			  strBackgroundDesktop = "./images/" + season + "sale/" + year + "/page_bg_english.gif";
+			} else {
+			  strBackgroundDesktop = "./summersale2023/home_header_bg_day_notext.gif";
+			}
+			$('.page_pattern_holder').addClass(season+ ' y'+ year);
+			$('.page_pattern_holder').removeClass('night');
+		  }
+	  
+		  //set BG
+		  $('.page_background_holder').css('background-image', 'url(' + strBackgroundDesktop + ')');
+		}
+	  
+		else {
+		  //set default BG
+		  let strBackgroundDesktop = "./summersale2023/home_header_bg_day_notext.gif";
+
+		  //its night
+		  if ( fn.timeofday === 'night') {
+			  strBackgroundDesktop = "./summersale2023/home_header_bg_night_notext.gif";
+			  $('.page_pattern_holder').addClass('night');
+		  }
+		  //its day
+		  else {
+			  strBackgroundDesktop = "./summersale2023/home_header_bg_day_notext.gif"
+			  $('.page_pattern_holder').removeClass('night');
+		  }
+		  $( '.page_background_holder' ).css( 'background-image', 'url(' + strBackgroundDesktop + ')' );
+		}
+	}
+
+	intervals() {
+		setInterval(this.updateDateTime, 1000);
+		setInterval(this.updateWeather, 3600000);
+		setInterval(this.updateBG, 3600000);
+	}
+
+	init() {
+		this.param = this.getURLParameters();
+		this.updateDateTime();
+		this.updateWeather();
+		this.updateBG(this.season, this.year);
+		this.intervals();
+
+	}
+	
 }
-
-// Usage:
-let param = getURLParameters();
-
-let locale = param.locale ? param.locale : 'hu-HU', //en-US
-    lat = param.lat ? param.lat : '47.2309',
-    lng = param.lng ? param.lng : '16.621',
-    hour12set = param.hour12 ? param.hour12 : false,
-    season = param.season ? param.season : 'summer',
-    year = param.year ? param.year : '2023';
-let openMeteoURL = 'https://api.open-meteo.com/v1/forecast?latitude='+lat+'&longitude='+lng+'&current_weather=true&timezone=auto';
-let weather,
-    formattedWeather;
-
-function updateWeather() {
-    $.getJSON(openMeteoURL, function(data) {
-        weather = data;
-        formattedWeather = weather['current_weather']['temperature']+'°C';
-    });
-}
-
-
-function updateDateTime() {
-    var date = new Date();
   
-    var options = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    var formattedDate = date.toLocaleDateString(locale, options);
-    var formattedTime = date.toLocaleTimeString(locale, { hour12: hour12set });
-  
-    var dateTimeElement = document.getElementById("datetime");
-    dateTimeElement.innerHTML = formattedDate +'<br>'+ formattedTime +' - '+formattedWeather;
-    //matrixSetIcon('192.168.0.2', '50459', temp_bit);
-    //matrixTEXT = formattedTime;
-	//matrixSetText(matrixIP, matrixID, matrixColor+matrixTEXT);
-}
-
-function updateBG(season, year) {
-  if (season && year) {
-    //set default BG
-    let strBackgroundDesktop = "./images/"+season+"sale/"+year+"/home_header_bg_day_notext.gif";
-
-    //get current date
-    let curDate = new Date();
-
-    //its night
-    if (curDate.getHours() >= 18 || curDate.getHours() < 6) {
-      if (season === "summer") {
-        strBackgroundDesktop = "./images/"+season+"sale/"+year+"/home_header_bg_night_notext.gif";
-      }
-      if (season === "spring") {
-        strBackgroundDesktop = "./images/"+season+"sale/"+year+"/page_bg_english.gif";
-      } else {
-        strBackgroundDesktop = "./summersale2023/home_header_bg_night_notext.gif";
-      }
-      $('.page_pattern_holder').addClass(season+' y'+year);
-      $('.page_pattern_holder').addClass('night');
-    }
-
-    //its day
-    else {
-      if (season === "summer") {
-        strBackgroundDesktop = "./images/" + season + "sale/" + year + "/home_header_bg_day_notext.gif";
-      }
-      if (season === "spring") {
-        strBackgroundDesktop = "./images/" + season + "sale/" + year + "/page_bg_english.gif";
-      } else {
-        strBackgroundDesktop = "./summersale2023/home_header_bg_day_notext.gif";
-      }
-      $('.page_pattern_holder').addClass(season+ ' y'+ year);
-      $('.page_pattern_holder').removeClass('night');
-    }
-
-    //set BG
-    $('.page_background_holder').css('background-image', 'url(' + strBackgroundDesktop + ')');
-  }
-
-  else {
-    //set default BG
-    let strBackgroundDesktop = "./summersale2023/home_header_bg_day_notext.gif";
-    //get current date
-    let curDate = new Date();
-    //its night
-    if ( curDate.getHours() >= 18 || curDate.getHours() < 6 ) {
-        strBackgroundDesktop = "./summersale2023/home_header_bg_night_notext.gif";
-        $('.page_pattern_holder').addClass('night');
-    }
-    //its day
-    else {
-        strBackgroundDesktop = "./summersale2023/home_header_bg_day_notext.gif"
-        $('.page_pattern_holder').removeClass('night');
-    }
-    $( '.page_background_holder' ).css( 'background-image', 'url(' + strBackgroundDesktop + ')' );
-  }
-}
-// Refresh date and time every second (1000 milliseconds)
-setInterval(updateDateTime, 1000);
-setInterval(updateWeather, 3600000);
-setInterval(updateBG, 3600000);
-
 $(document).ready(function () {
-    param = getURLParameters();
-    updateDateTime();
-    updateWeather();
-    updateBG(season, year);
+	let bg = new SteamBG
+	bg.init()
 });
